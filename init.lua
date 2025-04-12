ea3647bdde0007c46a24f30a0058dc8b7bb9c784
Ankush Kalra <ankushkalra2@gmail.com>
2025-04-13 01:23:56 +0530

    initial commit

---
+++ init.lua
@@ -0,0 +1,14 @@
+require("config.lazy")
+require("config.remap")
+require("config.telescope")
+vim.cmd([[
+	set number
+	imap jk <esc>
+	nnoremap <leader>ev :vsplit $MYVIMRC<cr>
+	nnoremap <leader>sv :source $MYVIMRC<cr>
+]])
+vim.cmd[[colorscheme tokyonight]]
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
