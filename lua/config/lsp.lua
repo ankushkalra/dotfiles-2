986e19246bb53e9ada72a938911f4a8a4b64bad6
Ankush Kalra <ankushkalra2@gmail.com>
2025-04-29 13:38:31 +0530

    added nvim tree

---
+++ lua/config/lsp.lua
@@ -0,0 +1,1 @@
+vim.keymap.set('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,9 +1,20 @@
+local keymap = vim.keymap
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set('n', '<Leader>o', '<C-o>', { noremap = true, silent = true })
+
+keymap.set("n", "<leader>pv", vim.cmd.Ex)
+keymap.set('n', '<Leader>o', '<C-o>', { noremap = true, silent = true })
+
 vim.cmd([[
 	imap jk <esc>
 	nnoremap <leader>ev :vsplit $MYVIMRC<cr>
 	nnoremap <leader>sv :source $MYVIMRC<cr>
 ]])
 
+keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
+keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
