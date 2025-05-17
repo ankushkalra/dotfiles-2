ab943a16a38db2c267bbc35293b986a9ddbfd1a7
Ankush Kalra <ankushkalra2@gmail.com>
2025-05-17 21:13:16 +0530

    added lush and removed vscode theme

--- init.lua
+++ init.lua
@@ -2,7 +2,10 @@ require("config.lazy")
 require("config.editor")
 require("config.remap")
 require("config.telescope")
-require("config.tokyonight")
 require("config.gitsigns")
 require("config.lsp")
 require("config.term")
+-- Lua
+-- vim.api.nvim_command('command! ReloadTheme lua dofile(vim.g.colors_path .. "/my_theme.lua")')
+-- vim.o.termguicolors = true -- Enable true color support
+-- vim.cmd.colorscheme("my_theme")
