845ed8896147c965ece4154021d68955f6d7eb27
Ankush Kalra <ankushkalra2@gmail.com>
2025-05-31 21:07:00 +0530

    added nvim-hightlight-groups

--- init.lua
+++ init.lua
@@ -5,7 +5,39 @@ require("config.telescope")
 require("config.gitsigns")
 require("config.lsp")
 require("config.term")
+
 -- Lua
--- vim.api.nvim_command('command! ReloadTheme lua dofile(vim.g.colors_path .. "/my_theme.lua")')
--- vim.o.termguicolors = true -- Enable true color support
--- vim.cmd.colorscheme("my_theme")
+vim.o.termguicolors = true -- Enable true color support
+
+local c = require('vscode.colors').get_colors()
+require('vscode').setup({
+  -- Alternatively set style in setup
+  -- style = 'light'
+
+  -- Enable transparent background
+  transparent = true,
+
+  -- Enable italic comment
+  italic_comments = true,
+
+  -- Underline `@markup.link.*` variants
+  underline_links = true,
+
+  -- Disable nvim-tree background color
+  disable_nvimtree_bg = true,
+
+  -- Apply theme colors to terminal
+  terminal_colors = true,
+
+  -- Override colors (see ./lua/vscode/colors.lua)
+  color_overrides = {
+  },
+
+  -- Override highlight groups (see ./lua/vscode/theme.lua)
+  group_overrides = {
+    -- this supports the same val table as vim.api.nvim_set_hl
+    -- use colors from this colorscheme by requiring vscode.colors!
+    Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+  }
+})
+vim.cmd.colorscheme "vscode"
