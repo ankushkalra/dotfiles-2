107dfb661a7d5c1ed2a63b8f9bc27d7e8891e85f
Ankush Kalra <ankushkalra2@gmail.com>
2025-10-31 19:22:40 +0530

    added fugitive buffer check for all formatters

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,20 +1,24 @@
+function getFormatters(formatters)
+  return function(bufnr)
+    local bufname = vim.api.nvim_buf_get_name(bufnr)
+    if bufname:match("^fugitive://") then
+      return {}
+    else
+      return formatters
+    end
+  end
+end
+
 return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      lua = function(bufnr)
-        local bufname = vim.api.nvim_buf_get_name(bufnr)
-        if bufname:match("^fugitive://") then
-          return {}
-        else
-          return { "stylua" }
-        end
-      end,
-      c = { "clang-format" },
-      javascript = { "prettierd", "prettier", stop_after_first = true },
-      typescript = { "prettierd", "prettier", stop_after_first = true },
-      javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+      lua = getFormatters { "stylua" },
+      c = getFormatters { "clang-format" },
+      javascript = getFormatters { "prettierd", "prettier", stop_after_first = true },
+      typescript = getFormatters { "prettierd", "prettier", stop_after_first = true },
+      javascriptreact = getFormatters { "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = getFormatters { "prettierd", "prettier", stop_after_first = true },
     },
     format_on_save = {
       timeout_ms = 500,
